c9a10a49c7dfd2cbe3b611a22c3262dffb9192c1
baktash81 <baktash.ansari1381@gmail.com>
2023-10-18 14:14:30 +0330

    [ADD] : add fetch function for country and state

--- src/components/Signupform.jsx
+++ src/components/Signupform.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useCityCountry } from '../hooks/useCityCountry';
+import Select from 'react-select';
 
 
 const Validate = (formData) => {
@@ -65,7 +66,7 @@ const formatDate = (dateString) => {
   return `${year}-${month}-${day}`;
 };
 
-const SignupForm = () => {
+const SignupForm =  () => {
 
 
     const [formData, setFormData] = useState({
@@ -75,8 +76,47 @@ const SignupForm = () => {
       email: '',
       password: '',
       confirmPassword: '',
+      country: '',
+      state: '',
+      birthdate: '',
       gender: 0
     });
+
+    const options = [
+      { value: 'USA', label: 'United States' },
+      { value: 'CAN', label: 'Canada' },
+      // Add more countries as needed
+    ];
+    
+    const States = [
+      { value: 'NYC', label: 'New York City', country: 'USA' },
+      { value: 'LA', label: 'Los Angeles', country: 'USA' },
+      // Add more cities and their corresponding countries as needed
+    ];
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [countries, setCountries] = useState(null);
+    const [states, setStates] = useState(null);
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSelectedState] = useState("");
+    
+    useEffect(() => {
+      const fetch = async () => {
+        const response = await useCityCountry("country");
+        setCountries(response)
+      }
+      fetch();
+    }, []);
+
+    useEffect(() => {
+      setSelectedState("")
+      const fetch = async () => {
+        console.log(selectedCountry);
+        const response = await useCityCountry("state", selectedCountry.value);
+        setStates(response)
+      }
+      fetch();
+    }, [selectedCountry]);
 
     const handleChange = (e) => {
       const { name, value } = e.target;
@@ -100,7 +140,7 @@ const SignupForm = () => {
         <h2 className="mb-5  text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Create your account
         </h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form className="space-y-2" onSubmit={handleSubmit}>
         <div className="flex flex-wrap -mx-3 mb-4">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="firstname">
@@ -143,9 +183,9 @@ const SignupForm = () => {
             <input 
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
               type="date" 
-              id="dob" 
-              name="dob"
-              value={formData.dob}
+              id="birthdate" 
+              name="birthdate"
+              value={formData.birthdate}
               onChange={handleChange}
               required
             />
@@ -168,6 +208,52 @@ const SignupForm = () => {
                   <option value="2">Female</option>
                   <option value="3">Other</option>
               </select>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap -mx-3 mb-4">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="country">
+              Country
+            </label>
+            <Select
+              id="country"
+              name="country"
+              options= {countries && countries.map(country => ({
+                value: country.country_name,
+                label: country.country_name,
+              }))}
+              value={selectedCountry}
+              onChange={(selectedCountry) => {
+                setSelectedCountry(selectedCountry)
+                setFormData({ ...formData, ["country"]:  selectedCountry.value});
+              }}
+              isSearchable
+              placeholder="Select a country"
+              required
+            />
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="city">
+              State
+            </label>
+            <Select
+              id="state"
+              name="state"
+              options= {states && states.map(state => ({
+                value: state.state_name,
+                label: state.state_name,
+              }))}
+              value={selectedState}
+              onChange={(selectedState) => {
+                setSelectedState(selectedState)
+                setFormData({ ...formData, ["state"]:  selectedState.value});
+              }}
+              isSearchable
+              isDisabled = {selectedCountry == ""}
+              placeholder="Select an state"
+              required
+            />
           </div>
         </div>
         
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -9,11 +9,11 @@ const SignupPage = () => {
         <h2 className="mb-5 text-center text-5xl font-bold leading-9 tracking-tight text-gray-900">
             TrekDestiny
         </h2>
-        <div className="flex items-center justify-center  h-[34rem] lg:w-4/6 rounded-3xl">
+        <div className="flex items-center justify-center  h-[35.5rem] lg:w-4/6 rounded-3xl">
 
           <div className="w-1/2 h-full xl:block hidden rounded-l-lg bg-signupimage object-cover bg-cover bg-center"></div>
 
-          <div className="bg-white xl:w-1/2 h-[34rem] rounded-r-lg">
+          <div className="bg-white xl:w-1/2 h-[35.5rem] rounded-r-lg">
               <SignupForm/>
           </div>
 
