5a125f21f33c634aa3ba96c2f276db3ab08c0fd8
shakiba <shakiba.anarakifirooz@gmail.com>
2023-10-19 23:20:23 +0200

    changed UI

--- src/pages/Verification.jsx
+++ src/pages/Verification.jsx
@@ -1,19 +1,15 @@
 import Verification from "../components/Verification";
+
 const VerificationPage = () => {
-    return(
-    <div className="fixed w-full h-full items-center justify-center bg-slate-200 bg-cover">
-      
-      <div className="flex flex-col items-center justify-center min-h-screen ">
-
-          <div className="bg-white xl:w-1/2 rounded-lg">
-              <Verification/>
+    return (
+      <div className="fixed w-full h-full items-center justify-center bg-slate-200 bg-cover">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+          <div className="bg-white xl:w-30% rounded-lg p-4">
+            <Verification />
           </div>
-
+        </div>
       </div>
-    </div>
     );
-}
-
-
-
-export default VerificationPage
+  };
+  
+  export default VerificationPage
