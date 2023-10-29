caca253f8272e9fdcdcfdd26d99c80c999169886
shakiba <shakiba.anarakifirooz@gmail.com>
2023-10-29 07:20:29 +0100

    [ADD]:added create card form to it's modal

--- src/components/Profile/Modal.jsx
+++ src/components/Profile/Modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateCardForm from '../ CreateCardForm';
 
 const Modal = ({isVisible , onClose}) => {
   if (!isVisible) return null;
@@ -12,7 +13,7 @@ const Modal = ({isVisible , onClose}) => {
       <div className="w-[600px] flex flex-col">
         <button className="text-red-700 text-xl place-self-end" onClick={() => onClose()}>X</button>
         <div className="bg-white p-2 rounded">
-          test
+          <CreateCardForm/>
         </div>
       </div>
     </div>
