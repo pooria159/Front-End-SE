43bf01f63c6f5a8d29c0eebfa1106d1ce7588e50
baktash81 <baktash.ansari1381@gmail.com>
2023-10-31 19:24:13 +0330

    [FIX] : fix annc modal css

--- src/components/Profile/Modal.jsx
+++ src/components/Profile/Modal.jsx
@@ -9,10 +9,10 @@ const Modal = ({isVisible , onClose}) => {
   }
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" id='wrapper' onClick={handelClose}>
-      <div className="w-[600px] h-full flex flex-col">
+    <div className="z-50 fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center" id='wrapper' onClick={handelClose}>
+      <div className="w-[35rem]  h-auto flex flex-col">
         <button className="text-red-700 text-xl place-self-end" onClick={() => onClose()}>X</button>
-        <div className="bg-white p-2 rounded">
+        <div className="bg-white p-5 rounded">
           <CreateCardForm/>
         </div>
       </div>
