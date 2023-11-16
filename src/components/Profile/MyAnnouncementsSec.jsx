f558252148e4ae78124a706fbf25e3c59ec16981
baktash81 <baktash.ansari1381@gmail.com>
2023-11-16 09:15:12 +0330

    [ADD] : add loader to my anc section

--- src/components/Profile/MyAnnouncementsSec.jsx
+++ src/components/Profile/MyAnnouncementsSec.jsx
@@ -1,11 +1,21 @@
 import React,{useState, useEffect} from "react";
 import MyCard from "./MyCard";
 import { useMyCard } from '../../hooks/useMyCard';
+import ClipLoader from "react-spinners/ClipLoader";
 
 
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <ClipLoader color="#2563EB" size={150} />
+        </div>
+    );
+};
 
 const MyAnncSec = () => {
+
     const [cardData, setCardData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -13,6 +23,7 @@ const MyAnncSec = () => {
                 const response = await useMyCard();
                 console.log(response.data);
                 setCardData(response.data.Cards);
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error:', error);
             }
@@ -24,11 +35,11 @@ const MyAnncSec = () => {
 
     return(
         <div>
-            <div className="flex flex-wrap justify-center items-center">
+            {isLoading ? <Loading/> :<div className="flex flex-wrap justify-center items-center">
                 {cardData && cardData.map((data, index) => {
                     return <MyCard key={index} data ={data} />
                 })}  
-            </div>
+            </div>}
         </div>
     );
 }
