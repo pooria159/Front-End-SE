19fcfbb38781274a51a330623aef318e240c7e70
shakiba anaraki firooz <shakibaanarakifirooz@shakibas-MacBook-Pro.local>
2023-11-26 19:09:54 +0330

    [TEST]:added test for create card form

--- src/tests/CreateCard.test.jsx
+++ src/tests/CreateCard.test.jsx
@@ -2,6 +2,9 @@ import CreateCardForm from '../components/ CreateCardForm';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
+import { getByRole, getAllByText, getByDisplayValue } from '@testing-library/react';
+import { button } from '@material-tailwind/react';
+
 
 vi.mock('../hooks/useCreateCard');
 vi.mock('react-toastify');
@@ -18,46 +21,37 @@ test('updates form data state on description change', () => {
       target: { value: 'Test description' },
     });
     expect(screen.getByLabelText(/Description/i).value).toBe('Test description');
-
-    // fireEvent.change(screen.getByLabelText(/Start Date/i), {
-    //     target: { value: '2023-12-01' },
-    //   });
-    
-    //   // Check if the Start Date field value has been updated
-    //   expect(screen.getByLabelText(/Start Date/i).value).toBe('2023-12-01');
-    
   
   });
-  
 
-//   test('displays error message when required fields are not filled', () => {
-//     render(<BrowserRouter><CreateCardForm /></BrowserRouter>);
-
-//     const submitButton = screen.getByText(/Create!/i);
-//     fireEvent.click(submitButton);
-
-//     expect(screen.getByText(/Please fill in all required fields/i)).toBeInTheDocument();
-//   });
-
-//   test('submits the form correctly', async () => {
-//     const mockedUseCreateCard = useCreateCard as jest.MockedFunction<typeof useCreateCard>;
-//     mockedUseCreateCard.mockResolvedValue({ status: 200, data: { message: 'Card created successfully!' } });
-
-//     render(<BrowserRouter><CreateCardForm /></BrowserRouter>);
-
-//     // Populate form data
-//     fireEvent.change(screen.getByLabelText(/Description/i), {
-//       target: { value: 'Test description' },
-//     });
-//     // Add similar fireEvent.change calls for other input fields
-
-//     const submitButton = screen.getByText(/Create!/i);
-//     fireEvent.click(submitButton);
-
-//     // You might want to wait for the asynchronous operation to complete
-//     await waitFor(() => {
-//       expect(mockedUseCreateCard).toHaveBeenCalledWith(/* expected form data */);
-//       // Add more assertions based on your component behavior
-//     });
-//   });
 });
+
+    // The form renders correctly with all the required fields.
+    it('should render form with all required fields', () => {
+      // render(<CreateCardForm />);
+      render(<BrowserRouter><CreateCardForm /></BrowserRouter>);
+      expect(screen.getByText(/Country/i)).toBeInTheDocument();
+      expect(screen.getByText(/State/i)).toBeInTheDocument();
+      expect(screen.getByText(/City/i)).toBeInTheDocument();
+      expect(screen.getByText(/Description/i)).toBeInTheDocument();
+      expect(screen.getByText(/Start Date/i)).toBeInTheDocument();
+      expect(screen.getByText(/End Date/i)).toBeInTheDocument();
+      expect(screen.getByText(/TravelersCount/i)).toBeInTheDocument();
+      expect(screen.getByText(/Preferred Languages/i)).toBeInTheDocument();
+      // expect(button.getByText(/Create/i)).toBeInTheDocument();
+      const createButton = screen.getByRole('button', { name: /Create/i });
+      // Assert that the button is in the document
+      expect(createButton).toBeInTheDocument();
+    });
+
+    // Submitting the form without filling in all required fields displays an error message.
+    // it('should display error message when submitting form without filling in all required fields', () => {
+    //   render(<BrowserRouter><CreateCardForm /></BrowserRouter>);
+    //   render(<CreateCardForm />);
+    //   const createButton = screen.getByRole('button', { name: /Create/i });
+    //   fireEvent.click(createButton);
+    //   expect(toast.error).toHaveBeenCalledWith('Please fill in all required fields.', {
+    //     position: toast.POSITION.TOP_LEFT,
+    //   });
+    // });
+
