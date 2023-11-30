cbd59d876db3474f691ee8adc3876f1d0ca5f280
shakiba anaraki firooz <shakibaanarakifirooz@shakibas-MacBook-Pro.local>
2023-11-30 23:18:37 +0330

    [ADD]:added history

--- src/components/Chat/ChatRoom.jsx
+++ src/components/Chat/ChatRoom.jsx
@@ -1,118 +1,158 @@
-import React, { useEffect, useState } from "react";
-const wsurl = import.meta.env.VITE_WEBSOCKET_CHAT_URL;
+import React, { useEffect, useState, useRef } from "react";
 import getDecodedToken from "../../hooks/useDecodedToken";
+import { useGetChatMessageCount } from "../../hooks/useGetChatMessageCount";
+import { useGetChatHistory } from "../../hooks/useGetChatHistory";
+
+const wsurl = import.meta.env.VITE_WEBSOCKET_CHAT_URL;
 
 function ChatRoom() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [ws, setWs] = useState(null);
+  const myToken = getDecodedToken();
+  const myID = myToken.UserID;
+  const myUsername = myToken.UserName;
+  let currentpage = 1;
 
-  useState(() => {
-    // const id = getDecodedToken();
-    /////needs to be changed
-    const id1 = 3;
+  const messageListRef = useRef(null);
+
+  useEffect(() => {
+    const id1 = 1;
     const id = 2;
-    ///////
     let firstID, secondID;
 
-    if (id1 > id.UserID) {
-      firstID = id.UserID;
+    if (id1 > id) {
+      firstID = id;
       secondID = id1;
     } else {
       firstID = id1;
-      secondID = id.UserID;
+      secondID = id;
     }
-    ////////////needs to be changed
-    const socket = new WebSocket(wsurl + "/"+firstID+"/"+secondID);
-    socket.onopen = (event) => {
-      console.warn("WebSocket connection opened:", event);
-      setWs(socket);
+
+    const fetchData = async () => {
+      try {
+        const countResponse = await useGetChatMessageCount(firstID, secondID);
+        const messageCount = countResponse.data.count;
+        console.log("messages count is ", messageCount);
+
+        const historyResponse = await useGetChatHistory(
+          firstID,
+          secondID,
+          messageCount,
+          currentpage
+        );
+        setMessages(historyResponse.data.reverse());
+        console.log(messages);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle the error here (e.g., show an error message to the user)
+      }
     };
 
-    socket.onclose = (event) => {
-      console.error("WebSocket connection closed:", event);
-    };
+    fetchData();
 
-    socket.onerror = (event) => {
-      console.error("WebSocket error:", event);
-    };
-
-    socket.onmessage = (event) => {
-      console.log("new on message");
-      console.log(event);
-      const newMessage = JSON.parse(event.data);
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
-    };
+    const socket = new WebSocket(wsurl + "/" + firstID + "/" + secondID);
+    setWs(socket);
 
     return () => {
+      // Close the WebSocket connection when the component unmounts
       if (ws) {
         ws.close();
       }
     };
-  }, []); // Empty dependency array to run this effect only once
+  }, []); // Empty dependency array to ensure this effect runs only once
+
+  useEffect(() => {
+    if (ws) {
+      ws.onmessage = (event) => {
+        console.log("new on message");
+        console.log(event.data);
+        const newMessage = JSON.parse(event.data);
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+      };
+    }
+  }, [ws]);
+
+  useEffect(() => {
+    // Scroll to the bottom of the message list when messages change
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   const sendMessage = () => {
-    const id = getDecodedToken();
-    // console.log("send is called");
     if (message && ws) {
       const chatRequest = {
         message: message,
-        user_id: id.UserID,
-        username: id.UserName,
+        user_id: myID.UserID,
+        username: myID.UserName,
       };
 
-      // Send the chat request to the server
       ws.send(JSON.stringify(chatRequest));
-      // console.log("send is DONE");
-      // Update the local state
-      // setMessages([...messages, chatRequest]);
       setMessage("");
     }
   };
 
   return (
     <div className="bg-gray-100 p-4 rounded-lg shadow-lg w-full">
-      <h2 className="text-xl font-bold mb-4">Welcome !</h2>
-      <div className="message-list h-60 overflow-y-auto">
+      <div ref={messageListRef} className="message-list h-60 overflow-y-auto space-y-4">
         {messages.map((msg, index) => (
           <div
             key={index}
-            className="bg-white p-2 mb-2 rounded-md shadow-md w-3/4"
+            className={`chat-message ${
+              msg.username === myUsername ? "right" : "left"
+            }`}
           >
-            <strong>{msg.username}</strong>
-            <div>{msg.message} </div>
-            <div className="text-sm text-right c">({msg.time})</div>
+            <div className="flex items-end justify-end">
+              <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1">
+                <div>
+                  <span
+                    className={`px-4 py-2 rounded-lg inline-block ${
+                      msg.username === myUsername
+                        ? "rounded-br-none bg-blue-600 text-white"
+                        : "rounded-bl-none bg-gray-300 text-gray-600"
+                    }`}
+                  >
+                    <div>
+                      <strong>{msg.username}</strong>
+                    </div>
+                    {msg.message}
+                    <div className="text-sm text-right c">({msg.time})</div>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-      <div className="message-input mt-4 w-full flex items-center">
-        <input
+      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+        <div className="relative flex">
+          <input
             type="text"
-            className="flex-grow px-3 py-2 rounded-full border-2 border-gray-200 focus:outline-none focus:border-blue-500"
-            placeholder="Type your message..."
+            placeholder="Write your message!"
+            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            
-        />
-        {/* <input
-  type="text"
-  className="flex-grow px-3 py-2 rounded-full border-2 border-gray-200 focus:outline-none focus:border-blue-500"
-  placeholder="Type your message..."
-  value={message}
-  onChange={(e) => setMessage(e.target.value)}
-  onKeyPress={(e) => {
-    if (e.key === 'Enter') {
-      sendMessage();
-    }
-  }}
-/> */}
-        <button
-            className="ml-4 bg-blue-500 text-white px-2 py-2 rounded-full"
-            onClick={sendMessage}
-        >
-            Send
-        </button>
+          />
+          <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+              // onClick={sendMessage}
+            >
+              {/* ... (send icon SVG) */}
+            </button>
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+              onClick={sendMessage}
+            >
+              <span className="font-bold">Send</span>
+              {/* ... (send icon SVG) */}
+            </button>
+          </div>
         </div>
+      </div>
     </div>
   );
 }
--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,20 +1,15 @@
-import CreateCardForm from "../components/ CreateCardForm";
 import ChatRoom from "../components/Chat/ChatRoom";
 
-
 const ChatRoomPage = () => {
-    return(
-    // <div className="fixed w-full h-full items-center justify-center bg-slate-200 bg-cover">
-      <div className="flex flex-col items-center justify-center min-h-screen  ">
-        <div className="flex items-center justify-center  h-[35.5rem] lg:w-3/6 md:w-full rounded-3xl ">
-          <div className="bg-white p-0 h-[35.5rem] m-0 2xl:w-1/2 xl:w-4/6 md:w-5/6 sm:w-[25rem] lg:w-5/6 rounded-r-lg">
-              <ChatRoom/>
-          </div>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center h-[40rem] lg:w-3/6 md:w-full rounded-3xl">
+        <div className="bg-white p-0 h-[25rem] m-0 2xl:w-2/3 xl:w-4/6 md:w-5/6 sm:w-[25rem] lg:w-5/6 rounded-r-lg">
+          <ChatRoom />
         </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
-
-
-export default ChatRoomPage
+export default ChatRoomPage;
